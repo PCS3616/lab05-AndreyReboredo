1dc9bf2d0dddba8c14017975eb5b0968a05c1eeb
AndreyReboredo <andreyreboredo@usp.br>
2025-02-21 22:07:46 -0300

    Add files via upload
    Fatorial traduzido

---
+++ submission/fatorial.asm
@@ -0,0 +1,31 @@
+; 0100 == N ;;; ele mantem os valores dos regs, ele faz uma copia
+; 0102 == N! ;;; e nao uma transferencia
+; 0106 == 0001
+; 0108 == variavel de controle
+; 010A == auxiliar
+
+LD /100
+SC /200
+MM /100
+HM /000
+
+@ /200
+K  /0000 ; subrotina
+MM /108 ; guarda controle
+SB /106 ; subtrai 1 do AC
+JZ /214 ; guarda controle
+MM /108
+LD /100 ; load de N
+ML /108 ; multiplica N por N-1
+MM /100 ; guarda N*
+LD /108 ; load da variavel de controle
+JP /202 ; 
+LD /100
+RS /200
+
+@ /100
+K /0003
+
+@ /106
+K /0001
+
